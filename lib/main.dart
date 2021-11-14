f14a8649fe94d0387449b38c3af4e51ecb78f1ea
SriKynanti <kynanti.ti@gmail.com>
2021-11-14 14:20:35 +0700

    Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/login.dart';
+import 'package:flutter_app/screens/register.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Login(),
+      routes: {
+        '/Login': (context) => Login(),
+        '/Register': (context) => Register(),
+      },
+    );
+  }
+}
